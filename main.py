466d0534de705a9c7a0565f7ac7bceec5e35b5e9
AlexMartin23 <832007@mystma.org>
2021-02-03 16:19:28 +0000

    reversal try 1

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-#WRITE YOUR CODE IN THIS FILE
+def reversal(w):
+    word = w[len(w)-1]
+    for i in range(2,len(w)+1):
+        word = word + w[len(w)-i]
+    return word
+
+print(reversal("alex"))
